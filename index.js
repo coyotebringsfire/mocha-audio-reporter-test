e4d8de7370a39e1ea4958cc1d76d9a7ee7845b94
CoyoteBringsFire <aumkara@gmail.com>
2015-05-15 11:11:26 -0700

    split index.js to success.js and failure.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ var Mocha=require('mocha'),
 	sounds=require('mario-sounds');
 
 var mocha = new Mocha({
+	timeout: 10000,
     ui: 'bdd',
     reporter: "mocha-multi",
     reporterOptions: {
 	    "mocha-audio-reporter": {
 		    stdout: '/dev/null',
 		    options: {
-		    	onStart: sounds["smb_vine"],
-		        onPass: sounds["smb_coin"],
-		        onFail: sounds["smb_mariodie"],
 		        onSuiteFail: sounds["smb_gameover"],
 		        onSuitePass: sounds["smb_stage_clear"]
 		    }
@@ -21,5 +19,6 @@ var mocha = new Mocha({
 		}
 	}
 });
-mocha.addFile("test/index.js");
+mocha.addFile("test/success.js");
+mocha.addFile("test/failure.js");
 mocha.run();
